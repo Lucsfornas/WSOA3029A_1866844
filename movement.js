07c8382e972b02867869b9ba2268bbb73b9abd45
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-08-05 15:34:07 +0200

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,10 +1,22 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navlinkfade o.5s ease forwards ${index / 7 + 2}s`
+            }
+
+        });
     });
+
+
 }
 
 navSlide();
