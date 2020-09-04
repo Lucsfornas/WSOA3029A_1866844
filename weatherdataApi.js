311dcaee41984da696df0cc0f7e4ea0c454ce86f
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-09-04 16:14:55 +0200

    fix

--- weatherdataApi.js
+++ weatherdataApi.js
@@ -1,7 +1,7 @@
-fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=SGD&to=MYR", {
+fetch("https://covid-19-data.p.rapidapi.com/totals?format=json", {
     "method": "GET",
     "headers": {
-        "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
         "x-rapidapi-key": "a78061f88cmsh3a84dcb9ef4c1c2p1d23f7jsna50cfba9f86c"
     }
 })
@@ -11,5 +11,3 @@ fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=SGD&to=MYR",
     .catch(err => {
         console.log(err);
     });
-
-
