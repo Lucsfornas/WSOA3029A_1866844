f43ece848c51f19fa36fbfac0d2ba9270a5bd294
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-09-04 15:48:30 +0200

    Update weatherdataApi.js

--- weatherdataApi.js
+++ weatherdataApi.js
@@ -1,16 +1,15 @@
-const api = {
-    key: "8a71da0d39cee6e47fe8472e84a233a2",
-
-}
-async function getweather() {
-    const response = await fetch(`https://history.openweathermap.org/data/2.5/aggregated/year?q=Johannesburg&appid=${api.key}`);
-    const data = await response.json();
-    const { }
-}
-
-fetch(`https://history.openweathermap.org/data/2.5/aggregated/year?q=Johannesburg&appid=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }).then(displayResults);
+fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=SGD&to=MYR", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+        "x-rapidapi-key": "a78061f88cmsh3a84dcb9ef4c1c2p1d23f7jsna50cfba9f86c"
+    }
+})
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 
