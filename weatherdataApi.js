b14e97ee0d71cd80ad27d74e7c27adb9ecf21fc8
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-09-05 14:32:35 +0200

    fixes

--- weatherdataApi.js
+++ weatherdataApi.js
@@ -1,3 +1,5 @@
+
+
 fetch("https://covid-19-data.p.rapidapi.com/report/totals?date-format=YYYY-MM-DD&format=json&date=2020-07-21", {
     "method": "GET",
     "headers": {
@@ -16,7 +18,18 @@ fetch("https://covid-19-data.p.rapidapi.com/report/totals?date-format=YYYY-MM-DD
         console.log(err);
     });
 
-function appendData(data) {
 
+function appendData(data) {
+    const maincontainer = document.getElementById('act');
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Active ' + data[i].active;
+        div.innerHTML = 'Confirmed ' + data[i].confirmed;
+        div.innerHTML = 'Critical ' + data[i].critical;
+        div.innerHTML = 'Date ' + data[i].date;
+        div.innerHTML = 'Deaths ' + data[i].deaths;
+        div.innerHTML = 'Recovered ' + data[i].recovered;
+        maincontainer.appendChild(div);
+    }
 }
 
